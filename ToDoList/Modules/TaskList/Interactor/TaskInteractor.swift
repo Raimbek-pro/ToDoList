b995cba8448d5287dda4175a7d13a1bd453093ac
Raiymbek <76900558+Raimbek-pro@users.noreply.github.com>
2025-05-30 15:04:58 +0500

    implement fetchTasks to load todos from API

--- ToDoList/Modules/TaskList/Interactor/TaskInteractor.swift
+++ ToDoList/Modules/TaskList/Interactor/TaskInteractor.swift
@@ -8,14 +8,55 @@
 import Foundation
 
 protocol TaskListInteractorProtocol {
-    func fetchTasks() -> [TaskEntity]
+    func fetchTasks(completion:@escaping ([TaskEntity])-> Void)
 }
 
 class TaskListInteractor: TaskListInteractorProtocol {
-    func fetchTasks() -> [TaskEntity] {
-        return [
-                   TaskEntity(id: 1, title: "Сделать тестовое", description: "Отправить до дедлайна", creationDate: Date(), isCompleted: false),
-                   TaskEntity(id: 2, title: "Покрасить волосы", description: "Чтобы пройти мимо красиво", creationDate: Date(), isCompleted: true)
-               ]
+    func fetchTasks(completion : @escaping([TaskEntity])-> Void) {
+        DispatchQueue.global().async (execute:{
+            guard let url = URL(string:"https://dummyjson.com/todos") else {
+                DispatchQueue.main.async{
+                    completion([])
+                }
+                return
+            }
+            URLSession.shared.dataTask(with:url){data,response,error in
+                guard let data = data,
+                      let decoded = try? JSONDecoder().decode(TodoResponse.self , from: data) else {
+                    DispatchQueue.main.async{
+                        completion([])
+                    }
+                    return
+                }
+                
+                
+                let tasks = decoded.todos.map{
+                    TaskEntity(
+                        id :Int64($0.id),
+                        title: $0.todo,
+                        description: "User ID: \($0.userId)",
+                        creationDate: Date(),
+                        isCompleted: $0.completed
+                    )
+                }
+                
+                DispatchQueue.main.async {
+                    completion(tasks)
+                }
+            }.resume()
+        })
+    
+       
+
     }
+}
+struct TodoResponse: Codable {
+    let todos: [TodoItem]
+}
+
+struct TodoItem: Codable {
+    let id: Int
+    let todo: String
+    let completed: Bool
+    let userId : Int
 }
