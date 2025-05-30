8c5a87e0e0ff3ba7b9e8f0dea926b3e2da8f1af8
Raiymbek <76900558+Raimbek-pro@users.noreply.github.com>
2025-05-30 18:17:02 +0500

    coredata nsmanagediobjects created, coredata entity set for item

---
+++ Item+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Item+CoreDataProperties.swift
+//  ToDoList
+//
+//  Created by Райымбек Омаров on 30.05.2025.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Item {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Item> {
+        return NSFetchRequest<Item>(entityName: "Item")
+    }
+
+    @NSManaged public var id: Int64
+    @NSManaged public var title: String?
+    @NSManaged public var taskDescription: String?
+    @NSManaged public var creationDate: Date?
+    @NSManaged public var isCompleted: Bool
+
+}
+
+extension Item : Identifiable {
+
+}
---
+++ ToDoList/Modules/TaskList/CoreData/Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  ToDoList
+//
+//  Created by Райымбек Омаров on 30.05.2025.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+
+}
